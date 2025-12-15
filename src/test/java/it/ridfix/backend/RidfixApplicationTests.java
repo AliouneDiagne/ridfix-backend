ced5b836395b64c795197344fa21e1a4dec32929
AliouneDiagne <Merlinognagne2006@email.com>
2025-12-15 14:30:00 +0100

    intial comit: Ridfix Backend base structure

---
+++ src/test/java/it/ridfix/backend/RidfixApplicationTests.java
@@ -0,0 +1,13 @@
+package it.ridfix.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidfixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
